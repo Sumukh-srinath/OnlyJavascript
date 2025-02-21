10ce2fd6474b86698adc1c00d3d40e10cae2c437
Sumukh S <sumukhsrinath2000@gmail.com>
2025-02-21 12:54:50 +0530

    strict vs No strict in Modules

--- NamasteSeries/NamasteNodeJS/02ModuleExportCJS/file1.js
+++ NamasteSeries/NamasteNodeJS/02ModuleExportCJS/file1.js
@@ -1,2 +1,5 @@
 const sum = require('./file2')
+//CJS doesnt have strict mode and below line wont give error if executed
+x = "helowesfse"
+console.log(x)
 sum(10, 40)
--- NamasteSeries/NamasteNodeJS/04ExportUsingESM/file1.js
+++ NamasteSeries/NamasteNodeJS/04ExportUsingESM/file1.js
@@ -1,3 +1,6 @@
 import { sum } from './file2.js'
 //this is new thing similar to what we have in react and this is the future way of importing and exporting things
+
+//here we have strict mode in ESM
+x = "HEllooooooooooooo"
 sum(3, 4)
